333b78a59396e160b1cc480dce0d6e83c0bd0afd
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-11-16 11:42:53 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5D8392',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
