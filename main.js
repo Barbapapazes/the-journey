d16b17f1a0ba2c3102b4d74d2991b8329aa65584
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-11-24 09:08:06 +0100

    feat: add mobile version

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+const nav = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+
+navBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  nav.classList.toggle('hidden')
+})
